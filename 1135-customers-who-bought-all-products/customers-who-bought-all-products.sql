1baddf0cfed91db06593fc45f5045c0492779f5d
Apoorv Goswami <42252898+gos-apoorv@users.noreply.github.com>
2025-06-30 07:12:14 +0100

    Time: 486 ms (74.52%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,12 +1,11 @@
 -- Write your PostgreSQL query statement below
 
 With tmp as (
-    Select count(distinct product_key) as total_products
+    Select count(product_key) as total_products
     from Product 
 )
 Select c.customer_id 
 From Customer c
-Join Product p on c.product_key = p.product_key
 Cross Join tmp
 Group by 1
-Having count(distinct p.product_key) = min(tmp.total_products)
+Having count(distinct c.product_key) = min(tmp.total_products)
