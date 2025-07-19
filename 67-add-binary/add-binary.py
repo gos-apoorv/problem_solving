1ecaad8614566fe7cf1fce9c67bc675d6c39b1c2
Apoorv Goswami <42252898+gos-apoorv@users.noreply.github.com>
2025-07-19 10:15:21 +0100

    Time: 3 ms (44.82%) | Memory: 17.8 MB (73.39%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -12,15 +12,15 @@ class Solution:
         final_b = "".join(['0'] * (max_len - b_len)) + b
 
         final_str = ""
-        print("final_a=", final_a, " final_b=", final_b)
+        # print("final_a=", final_a, " final_b=", final_b)
         for i in range(max_len-1, - 1, -1):
-            print("i=", i, " carry=", carry, " final_a[i]=", final_a[i], " final_b[i]=", final_b[i])
+            # print("i=", i, " carry=", carry, " final_a[i]=", final_a[i], " final_b[i]=", final_b[i])
             sum = carry + int(final_a[i]) + int(final_b[i])
 
             curr_dig = sum % 2
             carry = sum // 2
             final_str = str(curr_dig) + final_str
-            print("carry=", carry, " curr_dig=", curr_dig, " final_str=", final_str)
+            # print("carry=", carry, " curr_dig=", curr_dig, " final_str=", final_str)
 
         if carry > 0:
             final_str = str(carry) + final_str
