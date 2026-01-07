b7e32d71ead956d1974ea3369521f9a8a7c9404a
Apoorv Goswami <42252898+gos-apoorv@users.noreply.github.com>
2026-01-07 22:43:29 +0000

    Time: 11 ms (91.75%), Space: 20.7 MB (5.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,25 +3,39 @@ class Solution:
         self.nums = []
         self.memo = []
 
-    def canJumpFromPosition(self, position):
-        if self.memo[position] != -1:
-            return self.memo[position] == 1
+# This is using Top down approach
+    # def canJumpFromPosition(self, position):
+    #     if self.memo[position] != -1:
+    #         return self.memo[position] == 1
 
-        furthest_jump = min(
-           position +  self.nums[position],
-           len(self.nums) - 1
-        )
+    #     furthest_jump = min(
+    #        position +  self.nums[position],
+    #        len(self.nums) - 1
+    #     )
 
-        for pos in range(position + 1, furthest_jump + 1):
-            if self.canJumpFromPosition(pos):
-                self.memo[pos]  = 1
-                return True
-        self.memo[position]  = 0
-        return False
+    #     for pos in range(position + 1, furthest_jump + 1):
+    #         if self.canJumpFromPosition(pos):
+    #             self.memo[pos]  = 1
+    #             return True
+    #     self.memo[position]  = 0
+    #     return False
 
+    # def canJump(self, nums: List[int]) -> bool:
+    #     self.nums = nums
+    #     self.memo = [-1] * len(nums)
+
+    #     self.memo[-1] = 1
+    #     return self.canJumpFromPosition(0)
+
+# This is using greedy aproach
     def canJump(self, nums: List[int]) -> bool:
-        self.nums = nums
-        self.memo = [-1] * len(nums)
+        num_len = len(nums)
+        last_pos = num_len - 1
 
-        self.memo[-1] = 1
-        return self.canJumpFromPosition(0)
+        for i in range(last_pos, -1, -1):
+
+            if nums[i] + i >= last_pos:
+                last_pos = i
+            
+        return last_pos == 0
+
